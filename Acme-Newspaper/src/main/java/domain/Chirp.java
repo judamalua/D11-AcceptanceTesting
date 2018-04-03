4777a8360d0f50fddd9dacfb94f26eb08042e1f6
MrMJ06 <mrmj1996@gmail.com>
2018-04-03 18:41:16 +0200

    Domain fixed

---
+++ Acme-Newspaper/src/main/java/domain/Chirp.java
@@ -0,0 +1,60 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Chirp extends DomainEntity {
+
+	// Constructors -----------------------------------------------------------
+
+	// Attributes -------------------------------------------------------------
+	private String	title;
+	private String	description;
+	private Date	moment;
+
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@NotBlank
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Past
+	@NotNull
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	// Relationships ----------------------------------------------------------
+}
