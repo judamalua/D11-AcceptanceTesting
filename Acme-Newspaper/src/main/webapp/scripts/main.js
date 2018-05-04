8e1b7350dbd54e146e82a40ab64ed3c75260b27a
Daniel Diment <danieldiment.gj@gmail.com>
2018-05-04 22:55:13 +0200

    Improved language selector

--- Acme-Newspaper/src/main/webapp/scripts/main.js
+++ Acme-Newspaper/src/main/webapp/scripts/main.js
@@ -13,3 +13,12 @@ function getMainDomain() {
 		return "/";
 	}
 }
+function changeLanguage(language) {
+	if (language === 1) {
+		document.cookie = "language=es;path=/";
+	}
+	if (language === 0) {
+		document.cookie = "language=en;path=/";
+	}
+	location.reload();
+}
